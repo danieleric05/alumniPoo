02a9c8dd8f2a710fa50c7eaac21712dbca8b7583
aboussou <aboussoudaniel@gmail.com>
2019-01-14 16:31:03 +0000

    today

---
+++ src/Formulair/Model/Users.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Formulair\Model;
+
+class Users extends \RedBeanPHP\SimpleModel
+{
+    public $iNoUser;
+    public $sLogin;
+    public $sPassword;
+    public $LastName;
+    public $FirstName;
+    public $iCity;
+    public $iStatus;
+    public $YearWouldGraduateIn;
+    public $iYearStart;
+    public $iYearEnd;
+    public $dCreation;
+
+}
